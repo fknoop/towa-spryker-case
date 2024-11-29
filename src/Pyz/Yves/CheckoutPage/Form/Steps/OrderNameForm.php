d6adcbedc4ba49d6321d023c47ae196c2325806f
Frederik Knoop <frederik.knoop@devfk.de>
2024-11-29 16:12:34 +0100

    TSC-1: Implement field validation and adjust styling

--- src/Pyz/Yves/CheckoutPage/Form/Steps/OrderNameForm.php
+++ src/Pyz/Yves/CheckoutPage/Form/Steps/OrderNameForm.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * This file is part of the Spryker Commerce OS.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
 declare(strict_types=1);
 
 namespace Pyz\Yves\CheckoutPage\Form\Steps;
@@ -7,14 +12,47 @@ namespace Pyz\Yves\CheckoutPage\Form\Steps;
 use Spryker\Yves\Kernel\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 /**
  * @method \Pyz\Yves\CheckoutPage\CheckoutPageConfig getConfig()
  */
 class OrderNameForm extends AbstractType
 {
+    /**
+     * @var string
+     */
     public const FIELD_ORDER_NAME = 'orderName';
-    public const GLOSSARY_LABEL_ORDER_NAME = 'page.checkout.order_name.label';
+    /**
+     * @var string
+     */
+    protected const GLOSSARY_LABEL_ORDER_NAME = 'page.checkout.order_name.label';
+    /**
+     * @var string
+     */
+    protected const GLOSSARY_VALIDATION_NOT_BLANK_MESSAGE = 'validation.not_blank';
+    /**
+     * @var string
+     */
+    protected const GLOSSARY_VALIDATION_MAX_LENGTH_MESSAGE = 'validation.max_length.plural';
+    /**
+     * @var string
+     */
+    protected const GLOSSARY_VALIDATION_LOWERCASE_NUMBERS_MESSAGE = 'validation.lowercase_numbers';
+    /**
+     * @var string
+     */
+    protected const GLOSSARY_HELP_TEXT_ORDER_NAME = 'page.checkout.order_name.help';
+    /**
+     * @var int
+     */
+    protected const VALIDATION_MAX_LENGTH = 30;
+    /**
+     * @var string
+     */
+    protected const VALIDATION_REGEX_PATTERN = '/^[a-z0-9]+$/';
 
     /**
      * @return string
@@ -45,6 +83,20 @@ class OrderNameForm extends AbstractType
         $builder->add(static::FIELD_ORDER_NAME, TextType::class, [
             'label' => self::GLOSSARY_LABEL_ORDER_NAME,
             'required' => true,
+            'help' => static::GLOSSARY_HELP_TEXT_ORDER_NAME,
+            'constraints' => [
+                new NotBlank([
+                    'message' => static::GLOSSARY_VALIDATION_NOT_BLANK_MESSAGE,
+                ]),
+                new Length([
+                    'max' => static::VALIDATION_MAX_LENGTH,
+                    'maxMessage' => static::GLOSSARY_VALIDATION_MAX_LENGTH_MESSAGE,
+                ]),
+                new Regex([
+                    'pattern' => static::VALIDATION_REGEX_PATTERN,
+                    'message' => static::GLOSSARY_VALIDATION_LOWERCASE_NUMBERS_MESSAGE,
+                ]),
+            ],
         ]);
 
         return $this;
